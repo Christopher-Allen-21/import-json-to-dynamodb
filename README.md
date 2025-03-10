2d8a3ce86523efb5a5462b6e2fb2e0efcdca35ba
Christopher Allen <callen7908@yahoo.com>
2025-03-09 22:42:54 -0400

    Initial setup for importing movies data

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# import-json-to-dynamodb
+# Import JSON to DynamoDb
+
+- **Objective** - To put json list of objects into DynamoDb tables
+
+# Reference Video
+
+- https://www.youtube.com/watch?v=_x8d8sfik8I
+
+# To Run
+
+- **tbd** -
+
+# Packages to Install/Setup
+
+- **python -m venv my_env** - create virtual environment to isolate project dependencies
+- **pip install boto3** - installs boto3 for AWS functionality
+- **pip install simplejson** - installs json functionality
---
+++ import-json-to-dynamodb.py
@@ -0,0 +1,71 @@
+import json
+import boto3
+import logging
+from datetime import datetime
+
+
+MOVIE_TABLE = 'movies'
+TV_SHOW_TABLE = 'tv-shows'
+
+S3_BUCKET = 'video-content-bucket-1'
+JSON_FILE = 'contentFeed.json'
+
+
+logger = logging.getLogger
+logger.setLevel(logging.INFO)
+
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(MOVIE_TABLE)
+current_date = datetime.today().strftime('%Y-%m-%d')
+
+def lambda_handler(event, context):
+    bucket = S3_BUCKET
+    key = JSON_FILE
+
+    response = s3.get_object(Bucket = bucket, Key = key)
+    content = response['Body']
+    jsonObject = json.loads(content.read())
+    logger.info(jsonObject['Movies'])
+
+    for movie in jsonObject['Movies']:
+        logger.info(movie['title'])
+        table.put_item(Item = {
+            'name': movie['title'], 
+            'year': movie['releaseDate'], 
+            'description': movie['longDescription'],
+            'thumbnailUrl': movie['thumbnail'], 
+            'rating': movie['rating'], 
+            'cast': movie['cast'], 
+            'director': movie['director'], 
+            'genres': movie['genres'], 
+            'duration': movie['content']['duration'], 
+            'videoType': movie['content']['videos']['videoType'], 
+            'videoUrl': movie['content']['videos']['url'], 
+            'trailerUrl': None,
+            'dateAdded': current_date, 
+            'lastWatched': None, 
+            })
+
+
+# {
+#     "title": "The Wolf of Wallstreet",
+#     "id": "",
+#     "longDescription": "New York stockbroker Jordan Belfort, who founded brokerage firm Stratton Oakmont while still in his early 20s, develops habits of wretched excess and corruption.",
+#     "thumbnail": "https://video-content-bucket-1.s3.amazonaws.com/thumbNails/theWolfOfWallstreet.jpg",
+#     "releaseDate": "2013",
+#     "rating": "R",
+#     "cast": "Leonardo DiCaprio, Jonah Hill, Margot Robbie, Matthew McConaughey",
+#     "director": "Martin Scorsese",
+#     "genres": [
+#         "All", "Action"
+#     ],
+#     "content": {
+#         "duration": 10740,
+#         "videos": [{
+#             "videoType": "mp4",
+#             "url": "https://video-content-bucket-1.s3.us-east-1.amazonaws.com/movies/The.Wolf.of.Wall.Street.2013.1080p.BluRay.x264.YIFY.mp4",
+#             "quality": "HD"
+#         }]			
+#     }
+# }
